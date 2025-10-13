bcee84ce593f712ded0107a978419ac266ea2432
Medmad710 <149252314+Medmad710@users.noreply.github.com>
2025-10-13 19:03:24 +0500

    made main.dart and hello world

---
+++ lib/hello_world.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class HelloWorldPage extends StatefulWidget {
+  @override
+  _HelloWorldPageState createState() => _HelloWorldPageState();
+}
+
+class _HelloWorldPageState extends State<HelloWorldPage> {
+  Color _textColor = Colors.black;
+  final Random _random = Random();
+
+  void _changeColor() {
+    setState(() {
+      _textColor = Color.fromARGB(
+        255,
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World!'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello, World!',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: _textColor,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _changeColor,
+              child: Text('Change Color'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'hello_world.dart';
+// import 'calculator.dart';
+// import 'todo.dart';
+// import 'weather.dart';
+// import 'photo_viewer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Multi App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HelloWorldPage(),
+    // CalculatorPage(),
+    // TodoPage(),
+    // WeatherPage(),
+    // PhotoViewerPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.code), label: 'Hello'),
+          BottomNavigationBarItem(icon: Icon(Icons.calculate), label: 'Calc'),
+          BottomNavigationBarItem(icon: Icon(Icons.check_box), label: 'ToDo'),
+          BottomNavigationBarItem(icon: Icon(Icons.cloud), label: 'Weather'),
+          BottomNavigationBarItem(icon: Icon(Icons.photo), label: 'Photos'),
+        ],
+      ),
+    );
+  }
+}
